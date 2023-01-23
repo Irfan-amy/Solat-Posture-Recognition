4f514a16ba2fd76882848ab16c5688ec565d3c9b
Irfan-amy <mirfan.amy@gmail.com>
2023-01-24 03:25:18 +0800

    Update back.jsx

--- solat_posture_recognition_web/pages/back.jsx
+++ solat_posture_recognition_web/pages/back.jsx
@@ -175,33 +175,9 @@ const Home = () => {
       <canvas ref={canvasRef} width={1280} height={720} />
       <div className="absolute bottom-2 flex flex-col-reverse self-end w-full">
         <div className="flex flex-row justify-center w-full py-8  gap-2">
+          
           <div className="flex-none">
-            <button
-              onClick={(e) => {
-                e.preventDefault();
-                setFlip(!flip);
-                getMedia(flip,null);
-              }}
-              className="self-center bg-slate-500 hover:bg-slate-700 text-white font-bold py-4 px-4 rounded-full"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
-                />
-              </svg>
-            </button>
-          </div>
-          <div className="flex-none">
-            <div className="self-center bg-blue-500 text-white font-bold py-4 px-4 rounded-full">
+            <div className="self-center bg-blue-500 text-white text-[2.5vh] font-bold py-[2vh] px-[4vh] rounded-full">
               Status
             </div>
           </div>
